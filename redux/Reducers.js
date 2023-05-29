9d661ec716daac822a489db2624c74efdbd2b97a
Yassine Atik <brucexx02@gmail.com>
2023-05-29 14:27:07 +0100

    Added Storage File

--- redux/Reducers.js
+++ redux/Reducers.js
@@ -1,25 +1,52 @@
 import { ADD_FAVORITE_RESTAURANT, DELETE_FAVORITE_RESTAURANT } from "./Actions";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 const initialState = {
     favoriteRestaurants: [],
 };
 
+AsyncStorage.getItem("favoriteRestaurants")
+    .then((data) => {
+        if (data) {
+            try {
+                initialState.favoriteRestaurants = JSON.parse(data);
+            } catch (error) {
+                console.error("Invalid JSON data in AsyncStorage:", error);
+            }
+        }
+    })
+    .catch((error) => {
+        console.error("Error retrieving data from AsyncStorage:", error);
+    });
+
 export const RestaurantReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_FAVORITE_RESTAURANT:
-            return {
+            const updatedAddState = {
                 ...state,
                 favoriteRestaurants: [
                     ...state.favoriteRestaurants,
                     action.payload,
                 ],
             };
+            AsyncStorage.setItem(
+                "favoriteRestaurants",
+                JSON.stringify(updatedAddState.favoriteRestaurants)
+            );
+            return updatedAddState;
+
         case DELETE_FAVORITE_RESTAURANT:
-            return {
+            const updatedDeleteState = {
                 ...state,
                 favoriteRestaurants: state.favoriteRestaurants.filter(
                     (restaurant) => restaurant._id !== action.payload._id
                 ),
             };
+            AsyncStorage.setItem(
+                "favoriteRestaurants",
+                JSON.stringify(updatedDeleteState.favoriteRestaurants)
+            );
+            return updatedDeleteState;
         default:
             return state;
     }
